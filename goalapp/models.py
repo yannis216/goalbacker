abbea9e45893e8883f85bce3ed224edb54e6ab3d
yannis216 <yannisbeuke@googlemail.com>
2016-06-28 15:01:14 +0200

    First goalapp implementation

---
+++ goalapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone
+
+class Goal(models.Model):
+    goal_setter = models.CharField(max_length=50)
+    goal_body = models.CharField(max_length= 100)
+    goal_deadline = models.DateTimeField(default=timezone.now)
