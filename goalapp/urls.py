fb2986e09c1906c2ea9af59d793ccdca69813448
yannis216 <yannisbeuke@googlemail.com>
2016-10-09 00:26:48 +0200

    Major Design Implementation

--- goalapp/urls.py
+++ goalapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.front_site, name='front_site'),
+    url(r'^add_info/(?P<pk>\d+)/$', views.add_info, name='add_info'),
     url(r'^user_goals/(?P<pk>\d+)/$', views.user_goals, name='user_goals'),
 ]
--- goalapp/views.py
+++ goalapp/views.py
@@ -6,22 +6,35 @@ from .forms import GoalForm, UserForm
 def front_site(request):
     if request.method == "POST":
         goal_form = GoalForm(request.POST)
-        user_form = UserForm(request.POST)
-        if all([goal_form.is_valid(), user_form.is_valid()]):
-            user = user_form.save()
+        if all([goal_form.is_valid()]):
             goal = goal_form.save(commit="False")
-            goal.user = user
             goal.save()
-            return redirect('user_goals' , pk=user.pk)
+            return redirect('add_info', pk=goal.pk)
         else:
-            user_form = UserForm(request.POST)
             goal_form = GoalForm(request.POST)
-            return render(request, 'goalapp/front_site.html',  {'user_form': user_form, 'goal_form': goal_form})
+            return render(request, 'goalapp/front_site.html',  {'goal_form': goal_form})
     else:
         goal_form = GoalForm()
-        user_form = UserForm()
-        return render(request, 'goalapp/front_site.html',  {'user_form': user_form, 'goal_form': goal_form})
+        return render(request, 'goalapp/front_site.html',  {'goal_form': goal_form})
 
 def user_goals(request, pk):
     user = get_object_or_404(User, pk=pk)
     return render(request, 'goalapp/user_goals.html', {'user': user})
+
+def add_info(request, pk):
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        goal = get_object_or_404(Goal, pk=pk)
+        if all([user_form.is_valid()]):
+            user = user_form.save(commit="False")
+            goal.user = user
+            user.save()
+            goal.save()
+            return redirect('user_goals', pk=user.pk)
+        else:
+            user_form = UserForm(request.POST)
+            return render(request, 'goalapp/front_site.html',  {'user_form': user_form, 'goal_form': goal_form})
+    else:
+        goal = get_object_or_404(Goal, pk=pk)
+        user_form = UserForm()
+        return render(request, 'goalapp/add_info.html',  {'user_form': user_form, 'goal': goal})
